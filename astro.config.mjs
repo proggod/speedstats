a688e380c3bc2a9cd3ec419df07bad0cc128c58b
Preston Garrison <git@postingstats.com>
2025-07-23 20:26:28 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: cloudflare()
+  output: 'static'
 });
